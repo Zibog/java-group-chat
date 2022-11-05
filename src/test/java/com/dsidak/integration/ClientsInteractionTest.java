3d2841bf4c157986378da3f7822711ac6608c3b7
Zibog <dmsidak@gmail.com>
2022-11-05 19:34:35 +0300

    Fix tests in CI

--- src/test/java/com/dsidak/integration/ClientsInteractionTest.java
+++ src/test/java/com/dsidak/integration/ClientsInteractionTest.java
@@ -21,14 +21,10 @@ public class ClientsInteractionTest implements Testable {
     private Server server;
 
     @BeforeEach
-    public void init() {
-        try {
-            // The java.net API will automatically allocate a free port
-            server = new Server(new ServerSocket(0));
-            new Thread(() -> server.start()).start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void init() throws IOException {
+        // The java.net API will automatically allocate a free port
+        server = new Server(new ServerSocket(0));
+        new Thread(() -> server.start()).start();
     }
 
     @Test
